6d45bce5cde8e5494c4caf254ed3b3e3f0e6b1b0
ThanhTien3103 <thanhtien31032002@gmail.com>
2023-07-13 02:07:51 +0700

    real data FoodCate

---
+++ api/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+const cors = require('cors');
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  config();
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cors());
+  await app.listen(4001);
+}
+bootstrap();
