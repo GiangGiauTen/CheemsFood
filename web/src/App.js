d6c98537fe1b0decb2d27ec9df08fbdb0359bdf1
Đào Nhật Minh <58623645+Minh-19102@users.noreply.github.com>
2023-07-14 21:54:00 +0700

    fix login logout func

--- web/src/App.js
+++ web/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
-import { Routes, BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
+import { Routes, BrowserRouter as Router, Route, Link, NavLink, useNavigate } from 'react-router-dom'
 import Home from './components/Home'
-import LoginForm from './components/DangNhap/DangNhap'
-import RegisterForm from './components/DangKy/DangKy'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 const App = () => {
 	return (
 		<div>
 			<Router>
 				<Routes>
-					<Route path='/' element={<LoginForm />} />
-					<Route exact path='/home' element={<Home />} />
+					<Route path='/login' element={<LoginForm />} />
+					<Route path='/' exact element={<Home />} />
 					<Route path='/register' element={<RegisterForm />} />
 				</Routes>
 			</Router>
--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -1,12 +1,4 @@
-import {
-	TeamOutlined,
-	HomeOutlined,
-	BarChartOutlined,
-	ContactsOutlined,
-	UserOutlined,
-	UserAddOutlined,
-	LogoutOutlined,
-} from '@ant-design/icons'
+import { TeamOutlined, HomeOutlined, BarChartOutlined, ContactsOutlined, LogoutOutlined } from '@ant-design/icons'
 
 import { Layout, Menu, theme, Button } from 'antd'
 import Nhom from './Nhom/Nhom'
@@ -15,9 +7,10 @@ import QuanLyCongThucYeuThich from './QuanLyCongThuc/QuanLyCongThucYeuThich'
 import QuanLyDoLuuTru from './QuanLyDoLuuTru/QuanLyDoLuuTru'
 import QuanLyDoCanMua from './QuanLyDoCanMua/QuanLyDoCanMua'
 import AddTeam from './Nhom/AddTeam'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import AdminView from './Admin/adminView'
 
 const { Header, Content, Sider } = Layout
 
@@ -29,109 +22,121 @@ function getItem(label, key, icon, children) {
 		label,
 	}
 }
-const items = [
-	getItem('Nhóm', '_1', <TeamOutlined />, [getItem('Danh sách', '1'), getItem('Thêm mới', '11')]),
-	getItem('Quản Lý Công Thức', '_2', <HomeOutlined />, [
-		getItem('Danh sách', '2'),
-		getItem('Quản Lý Công Thức Yêu Thích', '21'),
-	]),
-	getItem('Quản Lý Đồ Lưu Trữ', '3', <BarChartOutlined />),
-	getItem('Quản Lý Đồ Cần Mua', '_4', <ContactsOutlined />, [getItem('Danh sách', '4')]),
-]
+
 function Home() {
 	const navigate = useNavigate()
-
-	const handleLogout = async () => {
-		try {
-			// Gửi yêu cầu POST để xóa dữ liệu
-			await axios.post('http://localhost:4001/seed')
-
-			// Chuyển hướng người dùng về trang đăng nhập
-			navigate('/')
-		} catch (error) {
-			console.error('Đăng xuất thất bại:', error)
+	const [role, setRole] = useState('user')
+	useEffect(() => {
+		const userId = localStorage.getItem('userId')
+		if (!userId) {
+			navigate('/login')
 		}
-	}
+		setRole(localStorage.getItem('role'))
+	})
 	const [menuKey, setMenuKey] = useState('1')
 	const {
 		token: { colorBgContainer },
 	} = theme.useToken()
 
+	const items = [
+		getItem('Nhóm', '_1', <TeamOutlined />, [getItem('Danh sách', '1'), getItem('Thêm mới', '11')]),
+		getItem('Quản Lý Công Thức', '_2', <HomeOutlined />, [
+			getItem('Danh sách', '2'),
+			getItem('Quản Lý Công Thức Yêu Thích', '21'),
+		]),
+		getItem('Quản Lý Đồ Lưu Trữ', '3', <BarChartOutlined />),
+		getItem('Quản Lý Đồ Cần Mua', '_4', <ContactsOutlined />, [getItem('Danh sách', '4')]),
+		getItem(
+			<Button
+				type='primary'
+				onClick={() => {
+					localStorage.removeItem('userId')
+					localStorage.removeItem('name')
+					localStorage.removeItem('username')
+					localStorage.removeItem('role')
+					navigate('/')
+				}}>
+				Đăng xuất
+			</Button>,
+			'5',
+		),
+	]
 	return (
 		<div>
-			<Layout hasSider theme='light'>
-				<Sider
-					style={{
-						overflow: 'auto',
-						height: '100vh',
-						position: 'fixed',
-						left: 0,
-						top: 0,
-						bottom: 0,
-					}}>
-					<div className='demo-logo-vertical' />
-					<Menu
-						theme='dark'
-						mode='inline'
-						defaultSelectedKeys={['1']}
-						items={items}
-						onClick={(e) => setMenuKey(e.key)}
-					/>
-				</Sider>
-				<Layout
-					className='site-layout'
-					style={{
-						marginLeft: 200,
-					}}>
-					<Header
+			{role == 'admin' ? (
+				<AdminView />
+			) : (
+				<Layout hasSider theme='light'>
+					<Sider
 						style={{
-							padding: 0,
-							background: colorBgContainer,
-						}}
-					/>
-
-					<Content
-						style={{
-							margin: '24px 16px 0',
-							overflow: 'initial',
+							overflow: 'auto',
+							height: '100vh',
+							position: 'fixed',
+							left: 0,
+							top: 0,
+							bottom: 0,
 						}}>
-						{menuKey === '1' && (
-							<div>
-								<Nhom />
-							</div>
-						)}
-						{menuKey === '11' && (
-							<div>
-								<AddTeam />
-							</div>
-						)}
-						{menuKey === '2' && (
-							<div>
-								<QuanLyCongThuc />
-							</div>
-						)}
-						{menuKey === '21' && (
-							<div>
-								<QuanLyCongThucYeuThich />
-							</div>
-						)}
+						<div className='demo-logo-vertical' />
+						<Menu
+							theme='dark'
+							mode='inline'
+							defaultSelectedKeys={['1']}
+							items={items}
+							onClick={(e) => setMenuKey(e.key)}
+						/>
+					</Sider>
+					<Layout
+						className='site-layout'
+						style={{
+							marginLeft: 200,
+						}}>
+						<Header
+							style={{
+								padding: 0,
+								background: colorBgContainer,
+							}}
+						/>
 
-						{menuKey === '3' && (
-							<div>
-								<QuanLyDoLuuTru />
-							</div>
-						)}
-						{menuKey === '4' && (
-							<div>
-								<QuanLyDoCanMua />
-							</div>
-						)}
-					</Content>
+						<Content
+							style={{
+								margin: '24px 16px 0',
+								overflow: 'initial',
+							}}>
+							{menuKey === '1' && (
+								<div>
+									<Nhom />
+								</div>
+							)}
+							{menuKey === '11' && (
+								<div>
+									<AddTeam />
+								</div>
+							)}
+							{menuKey === '2' && (
+								<div>
+									<QuanLyCongThuc />
+								</div>
+							)}
+							{menuKey === '21' && (
+								<div>
+									<QuanLyCongThucYeuThich />
+								</div>
+							)}
+
+							{menuKey === '3' && (
+								<div>
+									<QuanLyDoLuuTru />
+								</div>
+							)}
+							{menuKey === '4' && (
+								<div>
+									<QuanLyDoCanMua />
+								</div>
+							)}
+						</Content>
+					</Layout>
 				</Layout>
-				<Button type='primary' onClick={handleLogout}>
-					Đăng xuất
-				</Button>
-			</Layout>
+			)}
 		</div>
 	)
 }
