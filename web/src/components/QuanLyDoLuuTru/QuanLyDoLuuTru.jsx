037bd28965ce2259f46980a2a007070d96291294
dominhkhoi2002 <khoi.dominh2306@gmail.com>
2023-07-16 09:56:36 +0700

    temp

--- web/src/components/QuanLyDoLuuTru/QuanLyDoLuuTru.jsx
+++ web/src/components/QuanLyDoLuuTru/QuanLyDoLuuTru.jsx
@@ -3,11 +3,9 @@ import { Table, Space, Modal, Button, Form, Input } from 'antd';
 import axios from 'axios';
 import { API_URL } from '../../utils/apiUrl';
 
-
 const QuanLyDoLuuTru = () => {
   // Reserved foods state
   const [reservedFoods, setReservedFoods] = useState([]);
-
   const [userId, setUserId] = useState(4);
 
   useEffect(() => {
@@ -26,7 +24,7 @@ const QuanLyDoLuuTru = () => {
 
   const saveReservedFoods = async (foods) => {
     try {
-      await axios.post(`${API_URL}/storage/user/${userId}`, { foods }); // Pass the foods array as the request body
+      await axios.patch(`${API_URL}/storage/user/${userId}`, { foods }); // Use PATCH method to update the reserved foods
       console.log('Reserved foods saved successfully');
     } catch (error) {
       console.error('Error saving reserved foods:', error);
@@ -35,30 +33,30 @@ const QuanLyDoLuuTru = () => {
 
   // Define the columns for the table
   const columns = [
-	{
-		title: 'ID',
-		dataIndex: 'food.foodId',
-		key: 'foodId',
-		render: (text, record) => record.food.foodId, // Render the food ID value
-	  },
-	  {
-		title: 'Food Name',
-		dataIndex: 'food.name',
-		key: 'name',
-		render: (text, record) => record.food.name, // Render the food name value
-	  },
-	  {
-		title: 'Storage Date',
-		dataIndex: 'storageDate',
-		key: 'storageDate',
-		render: (date) => new Date(date).toLocaleDateString(), // Format the date
-	  },
-	  {
-		title: 'Outdate',
-		dataIndex: 'outdate',
-		key: 'outdate',
-		render: (date) => new Date(date).toLocaleDateString(), // Format the date
-	  },
+    {
+      title: 'ID',
+      dataIndex: 'food.foodId',
+      key: 'foodId',
+      render: (text, record) => record.food.foodId, // Render the food ID value
+    },
+    {
+      title: 'Food Name',
+      dataIndex: 'food.name',
+      key: 'name',
+      render: (text, record) => record.food.name, // Render the food name value
+    },
+    {
+      title: 'Storage Date',
+      dataIndex: 'storageDate',
+      key: 'storageDate',
+      render: (date) => new Date(date).toLocaleDateString(), // Format the date
+    },
+    {
+      title: 'Outdate',
+      dataIndex: 'outdate',
+      key: 'outdate',
+      render: (date) => new Date(date).toLocaleDateString(), // Format the date
+    },
     {
       title: 'Quantity',
       dataIndex: 'quantity',
@@ -69,9 +67,15 @@ const QuanLyDoLuuTru = () => {
       key: 'action',
       render: (text, record) => (
         <Space size="middle">
-          <a href="#1" onClick={() => handleView(record)}>View</a>
-          <a href="#2" onClick={() => handleEdit(record)}>Edit</a>
-          <a href="#3" onClick={() => handleDelete(record)}>Delete</a>
+          <a href="#1" onClick={() => handleView(record)}>
+            View
+          </a>
+          <a href="#2" onClick={() => handleEdit(record)}>
+            Edit
+          </a>
+          <a href="#3" onClick={() => handleDelete(record)}>
+            Delete
+          </a>
         </Space>
       ),
     },
@@ -95,7 +99,6 @@ const QuanLyDoLuuTru = () => {
   const handleEdit = (food) => {
     setSelectedFood(food);
     editForm.setFieldsValue({
-      description: food.food.description,
       quantity: food.quantity,
     });
     setEditModalVisible(true);
@@ -104,7 +107,9 @@ const QuanLyDoLuuTru = () => {
   // Handle delete action
   const handleDelete = (food) => {
     // Perform delete action here
-    setReservedFoods((prevFoods) => prevFoods.filter((item) => item.food.foodId !== food.food.foodId));
+    setReservedFoods((prevFoods) =>
+      prevFoods.filter((item) => item.food.foodId !== food.food.foodId)
+    );
   };
 
   // View modal close handler
@@ -115,43 +120,41 @@ const QuanLyDoLuuTru = () => {
 
   // Edit modal close handler
   const handleEditModalClose = () => {
-	setSelectedFood(null);
-	setEditModalVisible(false);
+    setSelectedFood(null);
+    setEditModalVisible(false);
   };
 
   const handleModalSubmit = () => {
-	editForm.validateFields().then((values) => {
-	  const updatedFood = {
-		...selectedFood,
-		food: {
-		  ...selectedFood.food,
-		  description: values.description,
-		},
-		quantity: values.quantity,
-	  };
-  
-	  if (values.quantity === 0) {
-		setReservedFoods((prevFoods) =>
-		  prevFoods.filter((food) => food.food.foodId !== updatedFood.food.foodId)
-		);
-	  } else {
-		setReservedFoods((prevFoods) =>
-		  prevFoods.map((food) => {
-			if (food.food.foodId === updatedFood.food.foodId) {
-			  return updatedFood;
-			}
-			return food;
-		  })
-		);
-	  }
-  
-	  setEditModalVisible(false);
-	});
+    editForm.validateFields().then((values) => {
+      const updatedFood = {
+        storageDate: selectedFood.storageDate,
+        outdate: selectedFood.outdate,
+        quantity: values.quantity,
+        foodId: selectedFood.food.foodId,
+      };
+
+      if (values.quantity === 0) {
+        // Remove the food from storage if quantity is 0
+        setReservedFoods((prevFoods) =>
+          prevFoods.map((food) =>
+            food.food.foodId === updatedFood.foodId ? { ...food, quantity: updatedFood.quantity } : food
+          ).filter((food) => food.quantity !== 0)
+        );
+      } else {
+        setReservedFoods((prevFoods) =>
+          prevFoods.map((food) =>
+            food.food.foodId === updatedFood.foodId ? { ...food, quantity: updatedFood.quantity } : food
+          )
+        );
+      }
+
+      setEditModalVisible(false);
+
+      // Update the reserved foods on the server
+      saveReservedFoods(reservedFoods);
+    });
   };
-  const editFormInitialValues = {
-	description: selectedFood?.food?.description,
-	quantity: selectedFood?.quantity,
-  };
+
   // Handle add action
   const handleAdd = () => {
     setSelectedFood(null);
@@ -178,144 +181,145 @@ const QuanLyDoLuuTru = () => {
   };
 
   return (
-	<div>
-	  <h1>Reserved Food List</h1>
-	  <Button type="primary" onClick={handleAdd}>
-		Add
-	  </Button>
-	  <Table columns={columns} dataSource={reservedFoods} />
-	  <Modal
-		title="Food Details"
-		visible={viewModalVisible}
-		onCancel={handleViewModalClose}
-		footer={[
-		  <Button key="close" onClick={handleViewModalClose}>
-			Close
-		  </Button>,
-		]}
-	  >
-		{selectedFood && (
-		  <Form layout="vertical">
-			<Form.Item label="Food Name">
-			  <Input value={selectedFood.food.name} readOnly />
-			</Form.Item>
-			<Form.Item label="Description">
-			  <Input.TextArea value={selectedFood.food.description} rows={4} readOnly />
-			</Form.Item>
-			<Form.Item label="Storage Date">
-			  <Input value={selectedFood.storageDate} readOnly />
-			</Form.Item>
-			<Form.Item label="Outdate">
-			  <Input value={selectedFood.outdate} readOnly />
-			</Form.Item>
-			<Form.Item label="Quantity">
-			  <Input value={selectedFood.quantity} readOnly />
-			</Form.Item>
-		  </Form>
-		)}
-	  </Modal>
-	  <Modal
-  title="Edit Food"
-  visible={editModalVisible}
-  onCancel={handleEditModalClose}
-  footer={[
-    <Button key="close" onClick={handleEditModalClose}>
-      Close
-    </Button>,
-    <Button key="save" type="primary" onClick={handleModalSubmit}>
-      Save
-    </Button>,
-  ]}
->
-  {selectedFood && (
-    <Form form={editForm} layout="vertical" initialValues={editFormInitialValues}>
-      <Form.Item name="description" label="Description">
-        <Input.TextArea rows={4} />
-      </Form.Item>
-      <Form.Item name="quantity" label="Quantity">
-        <Input type="number" min={0} />
-      </Form.Item>
-    </Form>
-  )}
-</Modal>
-	  <Modal
-		title="Add Food"
-		visible={addModalVisible}
-		onCancel={() => setAddModalVisible(false)}
-		onOk={handleAddModalSubmit}
-	  >
-		<Form form={newIngredientForm} layout="vertical">
-		  <Form.Item
-			name="name"
-			label="Food Name"
-			rules={[
-			  {
-				required: true,
-				message: 'Please enter the food name',
-			  },
-			]}
-		  >
-			<Input />
-		  </Form.Item>
-		  <Form.Item
-			name="description"
-			label="Description"
-			rules={[
-			  {
-				required: true,
-				message: 'Please enter the description',
-			  },
-			]}
-		  >
-			<Input.TextArea rows={4} />
-		  </Form.Item>
-		  <Form.Item
-			name="storage_date"
-			label="Storage Date"
-			rules={[
-			  {
-				required: true,
-				message: 'Please enter the storage date',
-			  },
-			]}
-		  >
-			<Input />
-		  </Form.Item>
-		  <Form.Item
-			name="outdate"
-			label="Outdate"
-			rules={[
-			  {
-				required: true,
-				message: 'Please enter the outdate',
-			  },
-			]}
-		  >
-			<Input />
-		  </Form.Item>
-		  <Form.Item
-			name="quantity"
-			label="Quantity"
-			rules={[
-			  {
-				required: true,
-				message: 'Please enter the quantity',
-			  },
-			  ({ getFieldValue }) => ({
-				validator(_, value) {
-				  if (!value || /^[0-9]+$/.test(value)) {
-					return Promise.resolve();
-				  }
-				  return Promise.reject('Please enter a valid integer for quantity');
-				},
-			  }),
-			]}
-		  >
-			<Input type="number" min={0} />
-		  </Form.Item>
-		</Form>
-	  </Modal>
-	</div>
+    <div>
+      <h1>Reserved Food List</h1>
+      <Button type="primary" onClick={handleAdd}>
+        Add
+      </Button>
+      <Table columns={columns} dataSource={reservedFoods} />
+      <Modal
+        title="Food Details"
+        visible={viewModalVisible}
+        onCancel={handleViewModalClose}
+        footer={[
+          <Button key="close" onClick={handleViewModalClose}>
+            Close
+          </Button>,
+        ]}
+      >
+        {selectedFood && (
+          <Form layout="vertical">
+            <Form.Item label="Food Name">
+              <Input value={selectedFood.food.name} readOnly />
+            </Form.Item>
+            <Form.Item label="Description">
+              <Input.TextArea value={selectedFood.food.description} rows={4} readOnly />
+            </Form.Item>
+            <Form.Item label="Storage Date">
+              <Input value={selectedFood.storageDate} readOnly />
+            </Form.Item>
+            <Form.Item label="Outdate">
+              <Input value={selectedFood.outdate} readOnly />
+            </Form.Item>
+            <Form.Item label="Quantity">
+              <Input value={selectedFood.quantity} readOnly />
+            </Form.Item>
+          </Form>
+        )}
+      </Modal>
+      <Modal
+        title="Edit Food"
+        visible={editModalVisible}
+        onCancel={handleEditModalClose}
+        footer={[
+          <Button key="close" onClick={handleEditModalClose}>
+            Close
+          </Button>,
+          <Button key="save" type="primary" onClick={handleModalSubmit}>
+            Save
+          </Button>,
+        ]}
+      >
+        {selectedFood && (
+          <Form form={editForm} layout="vertical" initialValues={selectedFood}>
+            <p>
+              <strong>Food Name:</strong> {selectedFood.food.name}
+            </p>
+            <Form.Item name="quantity" label="Quantity">
+              <Input type="number" min={0} />
+            </Form.Item>
+          </Form>
+        )}
+      </Modal>
+      <Modal
+        title="Add Food"
+        visible={addModalVisible}
+        onCancel={() => setAddModalVisible(false)}
+        onOk={handleAddModalSubmit}
+      >
+        <Form form={newIngredientForm} layout="vertical">
+          <Form.Item
+            name="name"
+            label="Food Name"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the food name',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="description"
+            label="Description"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the description',
+              },
+            ]}
+          >
+            <Input.TextArea rows={4} />
+          </Form.Item>
+          <Form.Item
+            name="storage_date"
+            label="Storage Date"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the storage date',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="outdate"
+            label="Outdate"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the outdate',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="quantity"
+            label="Quantity"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter the quantity',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || /^[0-9]+$/.test(value)) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('Please enter a valid integer for quantity');
+                },
+              }),
+            ]}
+          >
+            <Input type="number" min={0} />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
   );
 };
+
 export default QuanLyDoLuuTru;
