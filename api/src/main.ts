3a2bf6407a4155835ebe3327ef75e3bde5f6e101
minhdn-0255 <108458832+minhdn-0255@users.noreply.github.com>
2023-12-24 18:14:57 +0700

    update

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,15 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 import * as express from 'express';
-import { NestExpressApplication } from '@nestjs/platform-express'; // Import NestExpressApplication
+
+const cors = require('cors');
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule);
   config();
   app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      optionsSuccessStatus: 204
+    })
+  );
+
   app.use('/uploads', express.static('uploads'));
-  await app.listen(3000);
+  await app.listen(4001);
 }
 
 bootstrap();
