c73c656d8a4c44a58bae36dbd85270d292e5b490
minhdn-0255 <108458832+minhdn-0255@users.noreply.github.com>
2023-12-24 18:54:42 +0700

    update cors handle

---
+++ api/src/cors-handle/cors.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class CorsMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    res.header('Access-Control-Allow-Credentials', 'true');
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(204);
+    } else {
+      next();
+    }
+  }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -3,23 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 import * as express from 'express';
-
-const cors = require('cors');
+import { CorsMiddleware } from './cors-handle/cors.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   config();
   app.useGlobalPipes(new ValidationPipe());
-  app.use(
-    cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      optionsSuccessStatus: 204
-    })
-  );
-
+  app.use(CorsMiddleware);
   app.use('/uploads', express.static('uploads'));
-  await app.listen(4001);
+  await app.listen(3000);
 }
-
 bootstrap();
