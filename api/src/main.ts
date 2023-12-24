d1621a36cb8905fba8a72bbd74b7bc779f01615f
minhdn-0255 <108458832+minhdn-0255@users.noreply.github.com>
2023-12-24 14:24:37 +0700

    update method for deploy

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,9 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { config } from 'dotenv';
 import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express'; // Import NestExpressApplication
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'; // Import CorsOptions
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS
+  const corsOptions: CorsOptions = {
+    origin: true, // You can set specific origins or a boolean value
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  };
+
+  app.enableCors(corsOptions);
+
   config();
   app.useGlobalPipes(new ValidationPipe());
   app.use('/uploads', express.static('uploads'));
